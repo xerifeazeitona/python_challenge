d35334ed76301caddd639ef64a4d1d96eedb9427
xerifeazeitona <juliano.amaral@gmail.com>
2021-04-08 11:38:34 -0400

    Added level 0, warming up

---
+++ 00/0.py
@@ -0,0 +1,17 @@
+#import requests
+#res = requests.get('http://www.pythonchallenge.com/pc/def/0.html')
+#res.raise_for_status()
+#print(res.text)
+
+# Hint: try to change the URL address
+#res = requests.get('http://www.pythonchallenge.com/pc/def/1.html')
+#res.raise_for_status()
+#print(res.text)
+
+# New hint: 2**38 is much larger
+#res = requests.get(f'http://www.pythonchallenge.com/pc/def/{2**38}.html')
+#res.raise_for_status()
+#print(res.text)
+
+# Success, let's print out the next level url
+print('http://www.pythonchallenge.com/pc/def/map.html')
